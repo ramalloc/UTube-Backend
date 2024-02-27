a19e09ce970ca0440f9bc79fac1882944ee81b3a
ramalloc <roshangupta1887@gmail.com>
2024-02-27 18:47:15 +0530

    Made routes for controllers like changePassword, updateAccount, update avatar and coverImage user middleware here and getChannelDetails also watchHistory of user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
-import {upload}  from "../middlewares/multer.middleware.js"
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updatecoverImage } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -10,12 +10,12 @@ router.route("/register").post(upload.fields([
     {
         name: "avatar",
         maxCount: 1
-    }, 
+    },
     {
         name: "coverImage",
         maxCount: 1
     }
-]) ,registerUser);
+]), registerUser);
 
 // Login Route
 router.route("/login").post(loginUser);
@@ -27,6 +27,30 @@ router.route("/logout").post(verifyJWT, logoutUser);
 
 // refresh token route
 router.route("/refresh-token").post(refreshAccessToken);
+
+// Change password route
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+// Get Current user route
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+
+// Update Account Details route
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+// Update user avatar route
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+// Update user cover image route
+router.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updatecoverImage)
+
+// Getting details of user's channel route
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+
+// Getting watch history 
+router.route("/channel/watch-history").get(verifyJWT, getWatchHistory)
+
+
+
 
 export default router;
 
